55dec677926d7a544d1be2351a08953da3649b4b
kannaaaaaaaaaa <Kaeq2023@mymail.pomona.edu>
2024-11-01 14:06:47 -0700

    updated father in heaven and added Psalm23

--- OurFatherInHeaven.java
+++ OurFatherInHeaven.java
@@ -2,5 +2,6 @@ public class OurFatherInHeaven {
 
     public static void main(String[] args) {
         System.err.println("Hello wWorld!");
+        System.out.println("Thy will be done on Earth");
     }
 }
