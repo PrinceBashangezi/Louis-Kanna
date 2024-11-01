fc1ae97de3a8f49519bb15606f7f1e0d0719fc40
PrinceBashangezi <princebishogo@outlook.com>
2024-11-01 14:01:46 -0700

    Prince's changes

--- OurFatherInHeaven.java
+++ OurFatherInHeaven.java
@@ -1,5 +1,13 @@
 public class OurFatherInHeaven {
 
+    public boolean isEven(int number) {
+        if (number % 2 == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
     public static void main(String[] args) {
         System.err.println("Hello wWorld!");
     }
